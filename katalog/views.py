6cc31a09b0a33fcb6e7bc11dddc5daf215692cd0
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2022-09-08 22:31:06 +0700

    implement tugas2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
-# TODO: Create your views here.
+def show_catalog(req):
+    catalog_data = CatalogItem.objects.all()
+    print(catalog_data)
+    context = {
+        "nama": "Bonaventura Galang Kristabel Angipanglipur Hatmasasmita",
+        "npm": "2106630025",
+        "catalog_data": catalog_data
+    }
+    return render(req, "katalog/index.html", context)
