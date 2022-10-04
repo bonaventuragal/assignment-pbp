c224471aa53d6714effdd646e5968fd9c533ea74
Bonaventura Galang <77723914+bonaventuragal@users.noreply.github.com>
2022-10-04 22:08:30 +0700

    add responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./templates/**/*.html",
-            "./**/templates/*.html"],
-  theme: {
-    extend: {
-        colors: {
-            "orange-red": '#FF5E5B',
-            "gainsboro": '#D8D8D8',
-            "ivory": '#FFFFEA',
-            "robin-egg-blue": '#00CECB',
-            "corn": '#FFED66',
-            "quick-silver": '#A7A2A2',
-            "granny-smith-apple": "#80DE99"
-          }
+    important: true,
+    content: ["./templates/**/*.html",
+        "./**/templates/*.html"],
+    theme: {
+        extend: {
+            colors: {
+                "orange-red": '#FF5E5B',
+                "gainsboro": '#D8D8D8',
+                "ivory": '#FFFFEA',
+                "robin-egg-blue": '#00CECB',
+                "corn": '#FFED66',
+                "quick-silver": '#A7A2A2',
+                "granny-smith-apple": "#80DE99"
+            },
+        }
     },
-  },
-  plugins: [],
+    plugins: [],
 }
